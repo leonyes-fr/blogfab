b85a7e96e2c49b5294903004921ec3b532a4fc01
leonyes <vincent.aimevent@gmail.com>
2019-11-27 20:50:03 +0100

    base arbo ok !

---
+++ admin/adduser.php
@@ -0,0 +1,10 @@
+<?php
+
+
+    include('../config/config.php');
+    include('../librairies/db.lib.php');
+
+
+    include('tpl/layout.phtml');
+ 
+ ?>
---
+++ admin/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    include('../config/config.php');
+    include('../librairies/db.lib.php');
+
+    include('addUser.php');
+ 
+ ?>
