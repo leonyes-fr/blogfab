ff970b76751238d77ee7b4176476b94135115423
leonyes <vincent.aimevent@gmail.com>
2019-11-27 20:59:46 +0100

    prout

--- admin/adduser.php
+++ admin/adduser.php
@@ -1,9 +1,7 @@
 <?php
-
-
     include('../config/config.php');
     include('../librairies/db.lib.php');
-
+    
 
     include('tpl/layout.phtml');
  
--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,5 @@
 <?php
 
-    include('../config/config.php');
-    include('../librairies/db.lib.php');
 
     include('addUser.php');
  
