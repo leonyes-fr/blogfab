03871bb905a5a9fe65d16b05c9c900e45fc0ad7e
leonyes <vincent.aimevent@gmail.com>
2019-12-03 12:34:29 +0100

    prout safe mardi

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
 //Event sur click photo   A REVOIR ++++++++++
-$('.img-thumbnail').on('click', showImage);
+
+$(function() {
+    $('.img-thumbnail').on('click', showImage);
+    $('.delete').on('click',deleteArticle);
+}) ;
+
 
 function showImage(){
     document.querySelector('.pictureArticle').src = this.src;
     //$('.pictureArticle').attr('src', $(this).attr('src'));  Version jquery...
     $('#picture').modal('show'); // Puis on affiche la modal.
+}
+
+function deleteArticle(e){
+    e.preventDefault();
+    document.querySelector('.confirmDelete').href = this.href;
+    $('#deleteModal').modal('show'); // Puis on affiche la modal.
+    
 }
